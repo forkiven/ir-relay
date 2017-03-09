55ac8c7da619a2ecb51344bc17a02e6a85090c21
forkiven <mikewu1209@gmail.com>
2017-03-09 14:08:47 +0700

    strip whitespace

--- read/reader.py
+++ read/reader.py
@@ -73,7 +73,7 @@ while True:
 
 	# binaryString = "".join(map(lambda x: "1" if x[1] > 1000 else "0", filter(lambda x: x[0] == 1, command)))
 
-	if not(binaryString.isspace()):
+	if binaryString.strip():
 		print(binaryString)
 
 	#print "-----------End-----------\n"
