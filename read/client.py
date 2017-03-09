57b94228404682b0f44860feb292ae55e68af1eb
forkiven <mikewu1209@gmail.com>
2017-03-09 14:41:49 +0700

    removed old reader client code

--- read/client.py
+++ read/client.py
@@ -7,20 +7,4 @@ TCP_IP = '127.0.0.1'
 # Socket Port
 TCP_PORT = 6666
 
-# Buffer (amount of bytes)
-MESSAGE = "Hello, World!"
-
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect((TCP_IP, TCP_PORT))
-
-# messagesSent = 0
-
-while True:
-  s.send(MESSAGE)
-  time.sleep(5)
-  messagesSent += 1
-  
-  if messagesSent == 5:
-    break
-print("closing")
-s.close()
