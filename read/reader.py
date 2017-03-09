c904a82c94209ffc30234cbc7dadc411398ffb03
forkiven <mikewu1209@gmail.com>
2017-03-09 11:53:36 +0700

    Updated reader input pin

--- read/reader.py
+++ read/reader.py
@@ -6,7 +6,7 @@ from time import sleep
 
 # This is for revision 1 of the Raspberry Pi, Model B
 # This pin is also referred to as GPIO23
-INPUT_WIRE = 18
+INPUT_WIRE = 12
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(INPUT_WIRE, GPIO.IN)
